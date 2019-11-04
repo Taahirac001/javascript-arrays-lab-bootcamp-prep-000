1acf3309810de3679c5342f8464ae3174bcab9ac
Taahira Carter <taahirac001@gmail.com>
2019-11-04 02:12:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-  var appendKitten = kittens.pop(name)
+  var appendKitten = kittens.push(name)
   return appendKitten
 }
 
@@ -10,3 +10,4 @@ function destructivelyPrependKitten(name){
   var prependKitten = kittens.unshift(name)
   return prependKitten
 }
+
